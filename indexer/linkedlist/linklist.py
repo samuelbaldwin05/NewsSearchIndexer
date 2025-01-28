0ef150b5e2bd07dc867c8b5870730c570862f5fd
Michael Maaseide <maaseidemichael@gmail.com>
2025-01-27 21:37:04 -0500

    starting linked list

---
+++ indexer/linkedlist/linklist.py
@@ -0,0 +1,15 @@
+from ll_node import Node
+
+class LinkedList():
+    
+    def __init__(self):
+        self.head = None
+
+    def add(self, term, document_id):
+        pass
+
+    def search(self, term):
+        pass
+
+    def remove(self, term):
+        pass
---
+++ indexer/linkedlist/ll_node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self,term,doc_id):
+        self.term = term
+        self.doc_id = doc_id
+        self.next = None
