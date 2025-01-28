e28147bcdf3f23b4f7fe387aa97769e6c02e6098
Michael Maaseide <maaseidemichael@gmail.com>
2025-01-28 09:29:11 -0500

    Starting linked list creation

--- indexer/linkedlist/linklist.py
+++ indexer/linkedlist/linklist.py
@@ -6,10 +6,45 @@ class LinkedList():
         self.head = None
 
     def add(self, term, document_id):
-        pass
+        # Checking to see if term we are trying to add is in the list
+        current = self.head
+        while current is not None:
+            if current.term == term:
+                current.document_ids.add(document_id)
+                return
+            current = current.next
+
+        # If the node isn't in the list add it to the list
+        add = Node(term, document_id)
+        add.next = self.head
+        self.head = add
 
     def search(self, term):
-        pass
+        current = self.head
+        while current is not None:
+            if current.term == term:
+                return current.document_ids
+            current = current.next
+        return print('Node not in linked list.')
 
-    def remove(self, term):
-        pass
+    # def remove(self, term):
+    #     current = self.head
+    #     prev = None
+
+    #     while current:
+    #         if current.term == term:
+    #             if prev is None:
+    #                 self.head = current.next
+    #             else:
+    #                 prev.next = current.next
+    #             return True
+    #         prev = current
+    #         current = current.next
+
+    #     return print('Node not in linked list.')
+
+    # def display(self):
+    #     current = self.head
+    #     while current:
+    #         print(f"Term: {current.term}, Document IDs: {current.document_ids}")
+    #         current = current.next
--- indexer/linkedlist/ll_node.py
+++ indexer/linkedlist/ll_node.py
@@ -1,5 +1,17 @@
 class Node:
-    def __init__(self,term,doc_id):
+    def __init__(self,term,document_id):
         self.term = term
-        self.doc_id = doc_id
+        self.document_ids = [document_id]
         self.next = None
+    
+    def add_node(self,value):
+        self.document_ids.append(value)
+        
+    def get_values_count(self):
+        """
+        Returns the number of values stored in the node.
+
+        Returns:
+            int: The number of values stored in the node.
+        """
+        return len(self.values)
