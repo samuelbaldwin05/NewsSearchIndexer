141886c4298482750d9baad341846db1f8aeb1f4
Jeffrey Krapf <krapf.j@northeastern.edu>
2025-01-30 16:23:10 -0500

    Update hash_map.py

--- indexer/maps/hash_map.py
+++ indexer/maps/hash_map.py
@@ -1,16 +1,66 @@
-from indexer.abstract_index import AbstractIndex
 
-class HashMapIndex(AbstractIndex):
-    
-    def __init__(self):
-        super().__init__()
-        self.hash_map = {}
 
-    def add(self, term, document_id):
-        pass
-
-    def search(self, term):
-        pass
-
-    def remove(self, term):
-        pass
+  
+class HashMapIndex(): 
+    def __init__(self, capacity): 
+        self.capacity = capacity 
+        self.size = 0
+        self.table = [None] * capacity 
+  
+    def _hash(self, term): 
+        return hash(term) % self.capacity 
+  
+    def add(self, term, document_id): 
+        index = self._hash(term) 
+  
+        if self.table[index] is None: 
+            self.table[index] = Node(term, document_id) 
+            self.size += 1
+        else: 
+            current = self.table[index] 
+            while current: 
+                if current.term == term: 
+                    if document_id not in current.value:
+                        current.value.append(document_id) 
+                        return
+                    else:
+                        return
+                current = current.next
+            new_node = Node(term, document_id) 
+            new_node.next = self.table[index] 
+            self.table[index] = new_node 
+            self.size += 1
+  
+    def search(self, term): 
+        index = self._hash(term) 
+  
+        current = self.table[index] 
+        while current: 
+            if current.term == term: 
+                return (current.value)
+            current = current.next
+  
+        raise KeyError(term) 
+  
+    def remove(self, term): 
+        index = self._hash(term) 
+  
+        previous = None
+        current = self.table[index] 
+  
+        while current: 
+            if current.term == term: 
+                if previous: 
+                    previous.next = current.next
+                else: 
+                    self.table[index] = current.next
+                self.size -= 1
+                return
+            previous = current 
+            current = current.next
+ 
+class Node: 
+    def __init__(self, term, value): 
+        self.term = term 
+        self.value = [value] 
+        self.next = None
