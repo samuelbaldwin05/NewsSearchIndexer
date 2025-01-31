8b8c3e2dcc266f3a6817347945b49009fddb3a13
samuelbaldwin05 <sambaldwin2005@gmail.com>
2025-01-30 19:41:17 -0500

    Added meta data to indexing

--- assign_01.py
+++ assign_01.py
@@ -1,45 +1,54 @@
 import json
 import pickle
+import re
+from indexer.maps.hash_map import HashMapIndex
 from indexer.trees.avl_tree import AVLTreeIndex
 from indexer.trees.bst_index import BinarySearchTreeIndex
 from indexer.util.timer import timer
 from indexer.abstract_index import AbstractIndex
 from pathlib import Path
 
+def process_titles(title):
+    """ Given a title, process the title, splitting words into spaces, removing punctuation"""
+    title_cleaned = re.sub(r"[^A-Za-z\s]", "", title)
+    title_cleaned = title_cleaned.lower()
+    title_words = title_cleaned.split(" ")
+    return [title for title in title_words if len(title) > 0]
+
+def last_name(author_name):
+    """ Given an authors name, return their last name"""
+    names = author_name.split(" ")
+    return names[-1]
+
 @timer
 def index_files(path: str, index: AbstractIndex) -> None:
     path = Path(path)
 
     if path is not None:
-        print(f"Starting to index files in {path}")
+        print(f"path = {path}")
 
-    # Go through all subfolders
     for file in path.rglob("*.json"):
-        if file.is_file():
-            file_data = json.loads(file.read_text(encoding="utf-8"))
-            words = file_data["preprocessed_text"]
-            file_name = file.name
-            for word in words:
-                index.insert(word, file_name)
+        file_data = json.loads(file.read_text(encoding="utf-8"))
+        words = file_data["preprocessed_text"]
+        file_name = file.name
 
-# @timer
-# def index_words(path: str, index: AbstractIndex) -> None:
-#     path = Path(path)
-#
-#     if path is not None:
-#         print(f"path = {path}")
-#
-#     for file in path.glob("*.json"):
-#         file_data = json.loads(file.read_text(encoding="utf-8"))
-#         words = file_data["preprocessed_text"]
-#         file_name = file.name
-#         for word in words:
-#             index.insert(file_name, words)
+        # Index author last name
+        author_last_name = last_name(file_data["author_name"])
+        if len(author_last_name) != 0:
+            index.insert(author_last_name, file_name)
 
-# # A simple demo of how the @timer decoration can be used
-# @timer
-# def loopy_loop():
-#     total = sum((x for x in range(0, 1000000)))
+        # Index url
+        url = file_data["url"]
+        if len(url) != 0:
+            index.insert(url, file_name)
+
+        # Include list of preprocessed title words in words to be indexed
+        title = process_titles(file_data["title"])
+        if len(title) != 0:
+            words.append(title)
+
+        for word in words:
+            index.insert(word, file_name)
 
 def save_pickle(index, file_name):
     """ Given indexed key values and a desired file name, pickle the info for easy access later"""
@@ -52,48 +61,53 @@ def access_pickle(file_name):
         return pickle.load(file)
 
 def main():
-    # You'll need to change this to be the absolute path to the root folder
-    # of the dataset
-    # Sams directory
-    # data_directory = r"C:\Users\samba\OneDrive\Desktop\DS 4300 Large Scale Info\USFinancialNewsArticles-preprocessed\April2018"
-    # data_directory = r"C:\Users\samba\OneDrive\Desktop\DS 4300 Large Scale Info\P01-verify-dataset"
-    data_directory = r"/Users/jeffreykrapf/Desktop/ds4300/USFinancialNewsArticles-preprocessed"
-
-    # Here, we are creating a sample binary search tree index object
-    # and sending it to the index_files function
-    # bst_index = BinarySearchTreeIndex()
-    # index_files(data_directory, bst_index)
+    print(process_titles(""))
+    # # Sams directory
+    # data_directory = r"C:\Users\samba\OneDrive\Desktop\DS 4300 Large Scale Info\USFinancialNewsArticles-preprocessed"
+    data_directory = r"C:\Users\samba\OneDrive\Desktop\DS 4300 Large Scale Info\P01-verify-dataset"
+    #
+    # # Here, we are creating a sample binary search tree index object
+    # # and sending it to the index_files function
+    # # bst_index = BinarySearchTreeIndex()
+    # # index_files(data_directory, bst_index)
+    # #
+    # # # As a gut check, we are printing the keys that were added to the
+    # # # index in order.
+    # # print(bst_index.get_keys_in_order())
+    # #
+    # # search_word = 'act'
+    # # search_results = bst_index.search(search_word)
+    # # print(f"Files with {search_word}: {search_results}")
     #
     # # As a gut check, we are printing the keys that were added to the
     # # index in order.
     # print(bst_index.get_keys_in_order())
     #
-    # search_word = 'act'
-    # search_results = bst_index.search(search_word)
-    # print(f"Files with {search_word}: {search_results}")
-
-
-    # AVL Tree tests
-    avl_index = AVLTreeIndex()
-    #index_words(data_directory, avl_index)
-    index_files(data_directory, avl_index)
-    keys = avl_index.get_keys()
-    print(len(keys))
-    # print(keys)
-    print("Height:", avl_index._height(avl_index.root))
-
-    #search = 'preproc-news_0001728.json'
-    # search = 'act'
-    # search_results = avl_index.search(search)
-    # print(f"Files with {search}: {search_results}")
-    save_pickle(avl_index, "avlindex.pkl")
-    #loaded_index = access_pickle("avl_index.pkl")
-    #print(loaded_index.get_keys())
-    # quick demo of how to use the timing decorator included
-    # in indexer.util
-    # loopy_loop()
-
-
+    # # AVL Tree tests
+    # avl_index = AVLTreeIndex()
+    # #index_words(data_directory, avl_index)
+    # index_files(data_directory, avl_index)
+    # keys = avl_index.get_keys()
+    # print(len(keys))
+    # # print(keys)
+    # print("Height:", avl_index._height(avl_index.root))
+    #
+    # #search = 'preproc-news_0001728.json'
+    # # search = 'act'
+    # # search_results = avl_index.search(search)
+    # # print(f"Files with {search}: {search_results}")
+    # save_pickle(avl_index, "avlindex.pkl")
+    # #loaded_index = access_pickle("avl_index.pkl")
+    # #print(loaded_index.get_keys())
+    # # quick demo of how to use the timing decorator included
+    # # in indexer.util
+    # # loopy_loop()]
+    # # print('-' * 20)
+    # # hash_index = HashMapIndex(23)
+    # # index_files(data_directory, hash_index)
+    # # search = 'act'
+    # # search_results = hash_index.search(search)
+    # # print(f"Files with {search}: {search_results}")
 
 if __name__ == "__main__":
     main()
